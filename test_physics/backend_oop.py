5906d44bd7e7bc692f2650c00e9c8ae5ae1ba632
Verthandi <verthandi@me.com>
2014-11-16 10:37:41 +0800

    minor update

---
+++ test_physics/backend_oop.py
@@ -0,0 +1,19 @@
+class electron():
+    def __init__(self):
+        return None
+
+    @property
+    def spin(self):
+        return self.spin
+
+#    @property
+    def update(self, spin):
+        self.spin = spin
+
+# for i in range(L):
+#     for j in range(L):
+#         A[i, j] = electron()
+#         A[i, j].setValue()
+# loop = True
+# while loop:
+#     x = input()
