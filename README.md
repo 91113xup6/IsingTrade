a399bfc47f89ca416003925b6227772496cfdeef
Verthandi <verthandi@me.com>
2014-11-29 18:26:49 +0800

    modify directory structure

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ IsingTrade is a trading game designed for computational physics demonstration.
 ## Physics
 <img src="http://www.geocities.ws/shaista119/Ising1.jpg" title="Fetched from http://www.geocities.ws/shaista119">
 
-In Ising model, an array of electrons fluctuate according to the laws of quantum mechanics. This is useful in explaining the origin of magentism.
+In Ising model, an array of electrons fluctuate according to the laws of quantum mechanics. This is useful in explaining the origin of magnetism.
 
 ## Algorithmic Trading
 <img src="http://blog.xcelerit.com/wp-content/uploads/2013/07/algotrading.jpg" title="Fetched from http://blog.xcelerit.com">
--- test_physics/ising_simulation.py
+++ test_physics/ising_simulation.py
@@ -1,6 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import animation
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+import scipy.ndimage as ndi
+# from matplotlib import animation
+
+app = QtGui.QApplication([])
+
+win = pg.GraphicsWindow()
+win.setWindowTitle('Ising Model in 2D')
+vb = win.addViewBox()
+img = pg.ImageItem()
+vb.addItem(img)
+vb.setAspectLocked()
+
 
 T = 1
 fig = plt.figure()
@@ -15,13 +28,21 @@ for i in range(L):
 tmp = a
 
 
+def update():
+        img.setImage()
+
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)
+
+
 def neighbor(a, i, j):
-    tmp = []
-    tmp += [a[np.mod(i-1, L), j]]
-    tmp += [a[np.mod(i+1, L), j]]
-    tmp += [a[i, np.mod(j-1, L)]]
-    tmp += [a[i, np.mod(j+1, L)]]
-    return tmp
+        tmp = []
+        tmp += [a[np.mod(i-1, L), j]]
+        tmp += [a[np.mod(i+1, L), j]]
+        tmp += [a[i, np.mod(j-1, L)]]
+        tmp += [a[i, np.mod(j+1, L)]]
+        return tmp
 
 
 def dice(c, T):
@@ -63,3 +84,9 @@ for i in range(100):
 ani = animation.ArtistAnimation(fig, ims, interval=500*(1+1/(T+1)), blit=False)
 # ani.save('ising.mp4')
 plt.show()
+
+
+if __name__ == '__main__':
+        import sys
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+                QtGui.QApplication.instance().exec_()
