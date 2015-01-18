4ac25c2df35bee11d7ede2e3be880de307539d3f
Verthandi <verthandi@me.com>
2015-01-19 00:21:19 +0800

    web, monte_carlo, ...

--- src/phyeng.py
+++ src/phyeng.py
@@ -1,89 +1,93 @@
+#!/usr/bin/env python
+"""
+Usage: phyeng.py
+
+This program runs a 2D Ising Model infinitely.
+Spin and Value are sent through tcp://127.0.0.1:5557
+via send_pyobj, both are 2D np arrays.
+
+Communication is possible through tcp://127.0.0.1:5558
+Command:
+int -> temp
+'RESET' -> initialise
+'END' -> quit
+"""
+
 import numpy as np
 import zmq
-from itertools import chain
+import threading
 from time import sleep
+from scipy.stats import rv_discrete
 
-LENGTH = 30
+L = 60
+T = 1000
+
+def slice_(A):
+    return  np.sum(A.reshape(10, 6, -1, 6).swapaxes(1,2).reshape(-1, 6, 6),(1,2)).reshape(10,10) // 18
 
 
-def setSpin(r):
-    return np.random.choice([1]*(11+int(r*10)) + [0]*11)
+def energy(mat):
+    tot = np.sum(mat)*T
+    # tot += map()
+    return tot
 
 
-class electron():
-    def __init__(self, spin):
-        self.spin = spin
-        self.value = 200
+def iter_(mat):
+    sample = np.random.random((100, L, L))
+    sample = sample // np.tanh(T/2000.)
+    # probability = energy(np.logical_xor(mat, sample[i]))
+    sample_change = np.logical_not(np.logical_xor(mat, sample))
+    sample_energy = map(energy, sample_change)
+    mat_energy = energy(mat)
+    prob = np.exp(-(sample_energy-mat_energy)/T)
+    prob /= np.sum(prob)
+    dist = rv_discrete(values=(range(100), prob))
+    return sample_change[dist.rvs()]
 
-    @property
-    def spin(self):
-        return self.spin
+
+class lattice():
+    def __init__(self):
+        self.spin = np.random.random((L, L)) // 0.5
+        self.value = np.zeros((L, L))
+        self.value.fill(200)
 
     def updateValue(self):
         self.value += (self.spin * 2 - 1) * 10
-        if self.value < 0:
-            self.doomed = True
 
-    def updateSpin(self, r):
-        self.spin = setSpin(r)
-
-    @property
-    def doomed(self):
-        return self.doomed
+    def updateSpin(self):
+        self.spin = iter_(self.spin)
 
 
-A = np.ndarray((LENGTH, LENGTH), dtype=np.object)
-for i in xrange(LENGTH):
-    for j in xrange(LENGTH):
-        A[i, j] = electron(setSpin(0))
 
-def ratio(i, j):
-    s = 0
-    t = 0.
-    if i > 0:
-        s += A[i-1, j].spin
-        t += 1
-    if i < LENGTH-1:
-        s += A[i+1, j].spin
-        t += 1
-    if j > 0:
-        s += A[i, j-1].spin
-        t += 1
-    if j < LENGTH-1:
-        s += A[i, j+1].spin
-        t += 1
-    return s/t
-
-def update(lattice):
-    for i in range(LENGTH):
-        for j in range(LENGTH):
-            lattice[i, j].updateSpin(ratio(i, j))
-            lattice[i, j].updateValue()
+def phys_loop():
+    while True:
+        sleep(0.01)
+        A.updateValue()
+        A.updateSpin()
 
 
 def main():
+    global A
+    A = lattice()
+    t_phys = threading.Thread(target=phys_loop)
+    t_phys.start()
     context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://127.0.0.1:5557")
+    # data_channel = context.socket(zmq.PUSH)
+    # data_channel.bind("tcp://127.0.0.1:5557")
+    # command_channel = context.socket(zmq.PULL)
+    # command_channel.bind("tcp://127.0.0.1:5558")
     in_socket = context.socket(zmq.PULL)
     in_socket.connect("tcp://127.0.0.1:9241")
     out_socket = context.socket(zmq.PUSH)
-    out_socket.bind("tcp://127.0.0.1:9242")
+    out_socket.connect("tcp://127.0.0.1:9242")
     (message_type, session_id, data) = in_socket.recv_multipart()
     while True:
-        st = socket.recv(10)
-        if st == 'REQUEST':
-            print "Request accepted."
-            socket.send_pyobj([[[x.spin for x in y] for y in A], [[x.value for x in y] for y in A]])
-            print "Response sent."
-        update(A)
-        out_socket.send_multipart('message',
-                                  session_id,
-                                  ''.join(map(str, list(chain.from_iterable(A)))))
-        sleep(50)
-#        socket_web.send(A)
-#    socket_web.disconnect("tcp://127.0.0.1:5555")
-    socket.unbind("tcp://127.0.0.1:5557")
+        sleep(1)
+        print session_id
+        out_socket.send_multipart(['message', session_id,
+                                   ''.join( map(lambda x: str(int(x)), slice_(A.spin).flatten() ) )])
+    in_socket.close()
+    out_socket.close()
     context.destroy()
 
 if __name__ == '__main__':
