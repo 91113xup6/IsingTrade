3218935382ca17e0fe8799af621a891daaa2e501
Verthandi <verthandi@me.com>
2015-01-21 01:18:24 +0800

    numbajit

--- src/phyeng.py
+++ src/phyeng.py
@@ -15,40 +15,41 @@ int -> temp
 import numpy as np
 import zmq
 import threading
-from multiprocessing import Process
+# from multiprocessing import Process
 from time import sleep
 from scipy.stats import rv_discrete
-# from numba import jit
-L = 60
+# import matplotlib.pyplot as plt
+
+from numba import jit
+L = 20
 T = 10
+B = 1
 
 
-def slice_(A):
-    return np.sum(A.reshape(10, 6, -1, 6)
+def slice_(A, L):
+    return np.sum(A.reshape(10, L/10, -1, L/10)
                   .swapaxes(1, 2)
-                  .reshape(-1, 6, 6), (1, 2)).reshape(10, 10) // 18
+                  .reshape(-1, L/10, L/10), (1, 2)).reshape(10, 10) // (L**2/200)
 
-# @jit
-def iter_(mat, T):
-    # L = mat.shape[0]
+
+@jit
+def iter_(mat, T, B):
+    L = mat.shape[0]
     sample = np.random.random((100, L, L))
     sample = sample // np.tanh(T/2000.)
     sample_change = np.logical_not(np.logical_xor(mat, sample))
-    sample_energy = map(energy, sample_change)
-    mat_energy = energy(mat)
-    prob = np.exp(-(sample_energy-mat_energy)/T)
+    sample_energy = map(energy, sample_change, np.ones_like(sample_change)*B)
+    mat_energy = energy(mat, B)
+    prob = np.exp(-(sample_energy-mat_energy)*L**2/T/3600)
     prob /= np.sum(prob)
     dist = rv_discrete(values=(range(100), prob))
     return sample_change[dist.rvs()]
 
 
-# @jit
-def energy(mat):
-    # tot = np.sum(mat)*B
-    tot = 0
-    tot += np.sum([map(lambda x:np.sum(np.abs(np.diff(x))), mat)])
-    tot += np.sum([map(lambda x:np.sum(np.abs(np.diff(x))), mat.T)])
-    return tot
+@jit
+def energy(mat, B):
+    mat = mat * 2 - 1
+    return np.sum(mat)*B + np.sum(np.abs(np.diff(mat))) + np.sum(np.abs(np.diff(mat.T)))
 
 
 class lattice():
@@ -61,22 +62,32 @@ class lattice():
         self.value += (self.spin * 2 - 1) * 10
 
     def updateSpin(self):
-        self.spin = iter_(self.spin, T)
+        self.spin = iter_(self.spin, T, B)
 
 
 def phys_loop():
-    while True:
-        sleep(0.001)
-        A.updateValue()
-        A.updateSpin()
+    try:
+        while True:
+            sleep(0.001)
+            A.updateValue()
+            A.updateSpin()
+    except KeyboardInterrupt:
+        raise SystemExit
 
 
 def command_loop():
-    while True:
-        sleep(1)
-        # non blocking recv
+    try:
+        while True:
+            sleep(1)
+            # non blocking recv
+    except KeyboardInterrupt:
+        pass
+
 
 def main():
+    # plt.ion()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
     global A
     A = lattice()
     t_phys = threading.Thread(target=phys_loop)
@@ -98,9 +109,8 @@ def main():
             out_socket.send_multipart(['message', session_id,
                                        ''.join(map(lambda x: str(int(x)),
                                                    slice_(A.spin).flatten()))])
-    except EOFError:
+    except KeyboardInterrupt:
         pass
-        # t_phys.
     in_socket.close()
     out_socket.close()
     context.destroy()
